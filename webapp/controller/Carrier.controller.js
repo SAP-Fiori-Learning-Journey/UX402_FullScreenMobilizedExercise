78beb5d6945442035d65555445dbd1c76fa7d7ac
Modesto Mark Villanueva <mvillanuev29@dxc.com>
2020-09-16 01:09:35 +0000

    Final Solution

---
+++ webapp/controller/Carrier.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.sap.training.ux402.fullscreen.UX402_FullScreenExercise.controller.Carrier", {
+
+		getRouter: function () {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+
+		onPress: function (oEvent) {
+			
+			var oItem = oEvent.getSource();
+			var oCtx = oItem.getBindingContext();
+			var sCarrid = oCtx.getProperty("carrid");
+			
+			this.getRouter().navTo("flights", {
+				carrierId: sCarrid                
+			}, false);
+			
+		}
+
+	});
+
+});
---
+++ webapp/controller/Flights.controller.js
@@ -0,0 +1,50 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.sap.training.ux402.fullscreen.UX402_FullScreenExercise.controller.Flights", {
+
+		onInit: function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.getRoute("flights").attachMatched(this._onObjectMatched, this);
+		},
+		
+		onNavBack: function() {
+			
+			var oHistory, sPreviousHash, oRouter;
+			
+			oHistory = sap.ui.core.routing.History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+			
+			if(sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("overview", true);
+			}
+			
+		},
+		
+		_onObjectMatched: function(oEvent) {
+			
+			var oArgs = oEvent.getParameter("arguments");
+			this._sCarrierId = oArgs.carrierId;
+			
+			var oView = this.getView();
+			oView.bindElement({
+				path: "/CarrierCollection('" + this._sCarrierId + "')",
+				events: {
+					dataRequested: function() {
+						oView.setBusy(true);
+					},
+					dataReceived: function() {
+						oView.setBusy(false);
+					}
+				}
+			});
+		}
+
+	});
+
+});
